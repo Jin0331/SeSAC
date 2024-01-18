c5684e7e4d2f0174ec6711d43a0a9fd38ff8f96a
Jin0331 <sempre813@naver.com>
2024-01-18 11:03:26 +0900

    [feat] userDefualt를 활용한 Text Color 변경

--- Study/SeSAC4Network/SeSAC4Network/Translation/LanguageViewController.swift
+++ Study/SeSAC4Network/SeSAC4Network/Translation/LanguageViewController.swift
@@ -17,6 +17,10 @@ enum LanguageType {
 class LanguageViewController: UIViewController {
 
     @IBOutlet var tableView: UITableView!
+
+    var type : LanguageType = .source // 0: 원본 언어, 1: 목적언어
+    
+    var userSelect = "ko"
     
     let data = [
         "ko": "한국어",
@@ -25,11 +29,16 @@ class LanguageViewController: UIViewController {
         "fr": "프랑스어"
     ]
     
-    var type : LanguageType = .source // 0: 원본 언어, 1: 목적언어
+    lazy var list = data.sorted{ $0.key > $1.key}
     
     override func viewDidLoad() {
         super.viewDidLoad()
- 
+        
+        print(data)
+        print(data.sorted{$0.key > $1.key})
+        print(data.sorted{$0.key < $1.key})
+        print(data.sorted{$0.value > $1.value})
+        
         tableView.delegate = self
         tableView.dataSource = self
         
@@ -46,13 +55,19 @@ class LanguageViewController: UIViewController {
     } else {
      dismiss(animated: true)
     }*/
+        
+        print(userSelect)
                 
         switch type {
         case .source :
+            UserDefaults.standard.set(userSelect, forKey: "source")
             navigationController?.popViewController(animated: true)
         case .target :
+            UserDefaults.standard.set(userSelect, forKey: "target")
             dismiss(animated: true)
         }
+        
+        
     }
     
  
@@ -62,15 +77,32 @@ class LanguageViewController: UIViewController {
 extension LanguageViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
+        return list.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "languageCell") else { return UITableViewCell() }
         
+        // UserDefaults에 저장된 값에 따라 텍스트 컬러 변경
+        if let data = UserDefaults.standard.string(forKey: "source") {
+            if data == list[indexPath.row].key {
+                cell.textLabel?.textColor = .green
+            } else {
+                cell.textLabel?.textColor = .black
+            }
+        }
         
         
-        cell.textLabel?.text = data[indexPath.row]
+        cell.textLabel?.text = list[indexPath.row].value
+        
         return cell
+    }
+    
+    // userDeufalt를 이용한 값의 역전달
+    // 저장 버튼을 클릭했을 떄, userDefualt에 저장되도록
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        userSelect = list[indexPath.row].key
+        
     }
 }
